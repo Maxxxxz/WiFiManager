b9097ab9f6f31c3eaafcb7b0ea4dce044fe2ae8a
Michael Cooper <mc454916@ohio.edu>
2021-01-24 18:47:15 -0500

    Update strings to be more specific to project

--- strings_en.h
+++ strings_en.h
@@ -71,7 +71,7 @@ const char HTTP_FORM_PARAM_HEAD[]  PROGMEM = "<hr><h1>MyGunWasMoved Login Info</
 const char HTTP_FORM_PARAM[]       PROGMEM = "<br/><input id='{i}' name='{n}' maxlength='{l}' value='{v}' {c}>";
 
 const char HTTP_SCAN_LINK[]        PROGMEM = "<br/><form action='/wifi?refresh=1' method='POST'><button name='refresh' value='1'>Refresh</button></form>";
-const char HTTP_SAVED[]            PROGMEM = "<div class='msg'>Saving Credentials<br/>Trying to connect ESP to network.<br />If it fails reconnect to AP to try again</div>";
+const char HTTP_SAVED[]            PROGMEM = "<div class='msg'>Saving Credentials and attempting to pair device<br/>Trying to connect the device to network.<br />If it fails, reconnect to the device's network to try again.</div>";
 const char HTTP_PARAMSAVED[]       PROGMEM = "<div class='msg S'>Saved<br/></div>";
 const char HTTP_END[]              PROGMEM = "</div></body></html>";
 const char HTTP_ERASEBTN[]         PROGMEM = "<br/><form action='/erase' method='get'><button class='D'>Erase WiFi Config</button></form>";
